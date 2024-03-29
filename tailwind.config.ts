428f93b2fa3fc840fbe77a011e2b61b78e057760
Sam Selikoff <sam.selikoff@gmail.com>
2024-03-28 22:53:10 -0400

    wip

---
+++ tailwind.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from "tailwindcss";
+import colors from "tailwindcss/colors";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        gray: colors.slate,
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
